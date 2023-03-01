75c32ac8e43d3c4bb7c379e83e0c302529001229
pabparmen <pabparmen@alum.us.es>
2023-03-01 21:30:36 +0100

    student dashboard

---
+++ src/main/java/acme/forms/StudentDashboard.java
@@ -0,0 +1,36 @@
+
+package acme.forms;
+
+import acme.framework.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentDashboard extends AbstractForm {
+
+	//Serialisation identifier-----------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Attributes---------------------
+
+	protected Integer			totalNumberWoorkbookActivities;
+
+	protected Double			workbookAverage;
+
+	protected Double			workbookDeviation;
+
+	protected Double			workbookMaxPeriod;
+
+	protected Double			workbookMinPeriod;
+
+	protected Double			enrolmentAverage;
+
+	protected Double			enrolmentDeviation;
+
+	protected Double			enrolmentMaxTime;
+
+	protected Double			enrolmentMinTime;
+
+}
