bbad08512a965660a88d998042ddf19f584e65f9
pabparmen <pabparmen@alum.us.es>
2023-03-02 17:04:21 +0100

    workbook corregido

--- src/main/java/acme/entities/WorkBookActivities.java
+++ src/main/java/acme/entities/WorkBookActivities.java
@@ -6,10 +6,9 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -38,14 +37,14 @@ public class WorkBookActivities extends AbstractEntity {
 	protected String			abst;
 
 	@NotNull
-	protected ActivityType		activityType;
+	protected LessonType		activityType;
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@Future
-	@Past
+	@PastOrPresent
+	@NotNull
 	protected Date				timePeriod;
 
 	@URL
-	protected String			optionalLink;
+	protected String			link;
 
 }
