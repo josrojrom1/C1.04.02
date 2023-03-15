0c053dd98e2bc2cb035bf2c38ac3ba0531e5e5b1
pabparmen <pabparmen@alum.us.es>
2023-03-15 18:58:06 +0100

    Banner administrator

---
+++ src/main/java/acme/entities/Banner.java
@@ -0,0 +1,55 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Banner extends AbstractEntity {
+
+	//Serialisation identifier-----------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Attributes---------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date				moment;
+
+	@NotNull
+	@Past
+	@Valid
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				startDisplayPeriod;
+
+	@NotNull
+	@Valid
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				endDisplayPeriod;
+
+	@NotBlank
+	@URL
+	protected String			pictureLink;
+
+	@NotBlank
+	@URL
+	protected String			webLink;
+
+}
