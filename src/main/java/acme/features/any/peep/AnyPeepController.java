c5ad2ee63587f4aa1cae531ac1896630ec3643a8
pabparmen <pabparmen@alum.us.es>
2023-04-14 14:32:05 +0200

    peep operations

---
+++ src/main/java/acme/features/any/peep/AnyPeepController.java
@@ -0,0 +1,30 @@
+
+package acme.features.any.peep;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.Peep;
+import acme.framework.components.accounts.Any;
+import acme.framework.controllers.AbstractController;
+
+@Controller
+public class AnyPeepController extends AbstractController<Any, Peep> {
+
+	@Autowired
+	protected AnyPeepListService	listService;
+
+	@Autowired
+	protected AnyPeepShowService	showService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+		super.addBasicCommand("show", this.showService);
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/any/peep/list.jsp
@@ -0,0 +1,15 @@
+<%@page language="java"%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" uri="http://www.the-acme-framework.org/"%>
+
+<acme:list>
+	<acme:list-column code="any.peep.list.label.nick" path="nick" width="40%"/>
+	<acme:list-column code="any.peep.list.label.moment" path="moment" width="20%"/>
+	<acme:list-column code="any.peep.list.label.title" path="title" width="40%"/>
+	
+	
+	
+	
+</acme:list>
+
