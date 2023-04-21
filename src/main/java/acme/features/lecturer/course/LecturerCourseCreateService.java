0addccdaaccf0fb1b46e6c527bed4c0cb0307509
josej <josej@DESKTOP-2JBAL5I>
2023-04-21 09:09:36 +0200

    CourseValidations (code, retailPrice)

--- src/main/java/acme/features/lecturer/course/LecturerCourseCreateService.java
+++ src/main/java/acme/features/lecturer/course/LecturerCourseCreateService.java
@@ -51,6 +51,16 @@ public class LecturerCourseCreateService extends AbstractService<Lecturer, Cours
 
 	@Override
 	public void validate(final Course object) {
+
+		if (!super.getBuffer().getErrors().hasErrors("retailPrice"))
+			super.state(object.getRetailPrice().getAmount() >= 0, "retailPrice", "lecturer.lecture.form.error.retailPrice.positiveOrZero");
+
+		if (!super.getBuffer().getErrors().hasErrors("retailPrice"))
+			super.state(this.repository.findConfigurationAcceptedCurrencies().contains(object.getRetailPrice().getCurrency()), "retailPrice", "lecturer.lecture.form.error.retailPrice.currency");
+
+		if (!super.getBuffer().getErrors().hasErrors("code"))
+			super.state(!this.repository.findAllCodesFromCourses().contains(object.getCode()), "code", "lecturer.lecture.form.error.course.code.duplicated");
+
 		assert object != null;
 	}
 
