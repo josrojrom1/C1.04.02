39ebe672575fd47d81206354b0c05268295c32b7
josej <josej@DESKTOP-2JBAL5I>
2023-04-24 19:19:40 +0200

    OperationsByLecturerOnCourseANDLecture (ALL) OK!

--- src/main/java/acme/features/lecturer/course/LecturerCourseUpdateService.java
+++ src/main/java/acme/features/lecturer/course/LecturerCourseUpdateService.java
@@ -53,9 +53,6 @@ public class LecturerCourseUpdateService extends AbstractService<Lecturer, Cours
 		if (!super.getBuffer().getErrors().hasErrors("retailPrice"))
 			super.state(object.getRetailPrice().getAmount() <= 99999, "retailPrice", "lecturer.lecture.form.error.retailPrice.max");
 
-		if (!super.getBuffer().getErrors().hasErrors("code"))
-			super.state(!this.repository.findAllCodesFromCourses().contains(object.getCode()), "code", "lecturer.lecture.form.error.course.code.duplicated");
-
 		assert object != null;
 	}
 
