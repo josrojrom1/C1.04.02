ab768b744b11a16fb394433338a3055679e5307a
josej <josej@DESKTOP-2JBAL5I>
2023-05-12 16:35:14 +0200

    Test hacking list course rol Lecturer OK!

--- src/test/java/acme/testing/lecturer/course/LecturerCourseListTest.java
+++ src/test/java/acme/testing/lecturer/course/LecturerCourseListTest.java
@@ -1,6 +1,7 @@
 
 package acme.testing.lecturer.course;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -8,6 +9,7 @@ import acme.testing.TestHarness;
 
 public class LecturerCourseListTest extends TestHarness {
 
+	//Test POSITIVO
 	@ParameterizedTest
 	@CsvFileSource(resources = "/lecturer/course/list-all-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	public void test100Positive(final int recordIndex, final String code, final String title, final String price) {
@@ -20,6 +22,18 @@ public class LecturerCourseListTest extends TestHarness {
 		super.checkColumnHasValue(recordIndex, 2, price);
 		super.signOut();
 
+	}
+
+	//TEST HACKING
+	@Test
+	public void test200Hacking() {
+		super.checkLinkExists("Sign in");
+		super.request("/lecturer/course/list");
+		super.checkPanicExists();
+		super.signIn("administrator", "administrator");
+		super.request("/lecturer/course/list");
+		super.checkPanicExists();
+		super.signOut();
 	}
 
 }
