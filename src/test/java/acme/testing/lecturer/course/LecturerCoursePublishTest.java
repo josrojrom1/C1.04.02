be9df9dd57f2a9b0ad47b2d90a4d18680087fbdb
josej <josej@DESKTOP-2JBAL5I>
2023-05-12 20:28:31 +0200

    Publish course hacking test rol Lecturer OK!

--- src/test/java/acme/testing/lecturer/course/LecturerCoursePublishTest.java
+++ src/test/java/acme/testing/lecturer/course/LecturerCoursePublishTest.java
@@ -1,10 +1,14 @@
 
 package acme.testing.lecturer.course;
 
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.entities.Course;
 import acme.testing.TestHarness;
 
 public class LecturerCoursePublishTest extends TestHarness {
@@ -43,65 +47,65 @@ public class LecturerCoursePublishTest extends TestHarness {
 		super.checkNotErrorsExist();
 		super.signOut();
 	}
-	//
-	//	@Test
-	//	public void test300Hacking() {
-	//		// HINT: this test tries to publish a job with a role other than "Employer".
-	//
-	//		Collection<Job> jobs;
-	//		String params;
-	//
-	//		jobs = this.repository.findManyJobsByEmployerUsername("employer1");
-	//		for (final Job job : jobs)
-	//			if (job.isDraftMode()) {
-	//				params = String.format("id=%d", job.getId());
-	//
-	//				super.checkLinkExists("Sign in");
-	//				super.request("/employer/job/publish", params);
-	//				super.checkPanicExists();
-	//
-	//				super.signIn("administrator", "administrator");
-	//				super.request("/employer/job/publish", params);
-	//				super.checkPanicExists();
-	//				super.signOut();
-	//
-	//				super.signIn("worker1", "worker1");
-	//				super.request("/employer/job/publish", params);
-	//				super.checkPanicExists();
-	//				super.signOut();
-	//			}
-	//	}
+
+	@Test
+	public void test300Hacking() {
+		// HINT: this test tries to publish a course with a role other than "Employer".
+
+		final Collection<Course> courses;
+		String params;
+
+		courses = this.repository.findManyCoursesByLecturerUsername("lecturer1");
+		for (final Course course : courses)
+			if (course.isDraftMode()) {
+				params = String.format("id=%d", course.getId());
+
+				super.checkLinkExists("Sign in");
+				super.request("/lecturer/course/publish", params);
+				super.checkPanicExists();
+
+				super.signIn("administrator", "administrator");
+				super.request("/lecturer/course/publish", params);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("assistant1", "assistant1");
+				super.request("/lecturer/course/publish", params);
+				super.checkPanicExists();
+				super.signOut();
+			}
+	}
 	//
 	//	@Test
 	//	public void test301Hacking() {
-	//		// HINT: this test tries to publish a published job that was registered by the principal.
+	//		// HINT: this test tries to publish a published course that was registered by the principal.
 	//
-	//		Collection<Job> jobs;
+	//		Collection<Job> courses;
 	//		String params;
 	//
-	//		super.signIn("employer1", "employer1");
-	//		jobs = this.repository.findManyJobsByEmployerUsername("employer1");
-	//		for (final Job job : jobs)
-	//			if (!job.isDraftMode()) {
-	//				params = String.format("id=%d", job.getId());
-	//				super.request("/employer/job/publish", params);
+	//		super.signIn("lecturer1", "lecturer1");
+	//		courses = this.repository.findManyJobsByEmployerUsername("lecturer1");
+	//		for (final Job course : courses)
+	//			if (!course.isDraftMode()) {
+	//				params = String.format("id=%d", course.getId());
+	//				super.request("/lecturer/course/publish", params);
 	//			}
 	//		super.signOut();
 	//	}
 	//
 	//	@Test
 	//	public void test302Hacking() {
-	//		// HINT: this test tries to publish a job that wasn't registered by the principal,
+	//		// HINT: this test tries to publish a course that wasn't registered by the principal,
 	//		// HINT+ be it published or unpublished.
 	//
-	//		Collection<Job> jobs;
+	//		Collection<Job> courses;
 	//		String params;
 	//
-	//		super.signIn("employer2", "employer2");
-	//		jobs = this.repository.findManyJobsByEmployerUsername("employer1");
-	//		for (final Job job : jobs) {
-	//			params = String.format("id=%d", job.getId());
-	//			super.request("/employer/job/publish", params);
+	//		super.signIn("lecturer2", "lecturer2");
+	//		courses = this.repository.findManyJobsByEmployerUsername("lecturer1");
+	//		for (final Job course : courses) {
+	//			params = String.format("id=%d", course.getId());
+	//			super.request("/lecturer/course/publish", params);
 	//		}
 	//		super.signOut();
 	//	}
