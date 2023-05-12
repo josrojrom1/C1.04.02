49b55f33828c8a6923ed333501aec907ffb282fc
josej <josej@DESKTOP-2JBAL5I>
2023-05-13 00:06:01 +0200

    Publish lecture test (pos,neg,hack) rol Lecturer OK!

--- src/test/java/acme/testing/lecturer/lecture/LecturerLecturePublishTest.java
+++ src/test/java/acme/testing/lecturer/lecture/LecturerLecturePublishTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import acme.entities.Course;
+import acme.entities.Lecture;
 import acme.testing.TestHarness;
 
 public class LecturerLecturePublishTest extends TestHarness {
@@ -19,92 +19,82 @@ public class LecturerLecturePublishTest extends TestHarness {
 	// Test methods -----------------------------------------------------------
 
 
+	//TEST POSITIVO
 	@ParameterizedTest
-	@CsvFileSource(resources = "/lecturer/course/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/lecturer/lecture/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	public void test100Positive(final int recordIndex) {
-		// HINT: En este test nos autenticamos como Lecturer, listamos los courses, seleccionamos uno de ellos y lo publicamos
+		// HINT: En este test nos autenticamos como Lecturer, listamos las lectures, seleccionamos una de ellos y la publicamos
 		super.signIn("lecturer1", "lecturer1");
-		super.clickOnMenu("Lecturer", "List my courses");
+		super.clickOnMenu("Lecturer", "List my lectures");
 		super.checkListingExists();
-		super.clickOnListingRecord(1);
+		super.sortListing(0, "desc");
+		super.clickOnListingRecord(0);
 		super.checkFormExists();
 		super.clickOnSubmit("Publish");
 		super.checkNotErrorsExist();
-
 		super.signOut();
+
 	}
 
-	@ParameterizedTest
-	@CsvFileSource(resources = "/lecturer/course/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test200Negative(final int recordIndex) {
-		// HINT: En este test intentamos publicar un course el cual aun no puede ser publicado, en este caso porque no tiene lectures
-		super.signIn("lecturer1", "lecturer1");
-		super.clickOnMenu("Lecturer", "List my courses");
-		super.checkListingExists();
-		super.clickOnListingRecord(0);
-		super.checkFormExists();
-		super.checkNotButtonExists("Publish");
-		super.checkNotErrorsExist();
-		super.signOut();
-	}
-
+	//TEST HACKING 1
 	@Test
 	public void test300Hacking() {
-		// HINT: this test tries to publish a course with a role other than "Employer".
-
-		final Collection<Course> courses;
+		// HINT: En este test intentamos publicar una lecture con un rol distinto a Lecturer
+		final Collection<Lecture> lectures;
 		String params;
 
-		courses = this.repository.findManyCoursesByLecturerUsername("lecturer1");
-		for (final Course course : courses)
-			if (course.isDraftMode()) {
-				params = String.format("id=%d", course.getId());
+		lectures = this.repository.findManyLecturesByLecturerUsername("lecturer1");
+		for (final Lecture lecture : lectures)
+			if (lecture.isDraftMode()) {
+				params = String.format("id=%d", lecture.getId());
 
 				super.checkLinkExists("Sign in");
-				super.request("/lecturer/course/publish", params);
+				super.request("/lecturer/lecture/publish", params);
 				super.checkPanicExists();
 
 				super.signIn("administrator", "administrator");
-				super.request("/lecturer/course/publish", params);
+				super.request("/lecturer/lecture/publish", params);
 				super.checkPanicExists();
 				super.signOut();
 
 				super.signIn("assistant1", "assistant1");
-				super.request("/lecturer/course/publish", params);
+				super.request("/lecturer/lecture/publish", params);
 				super.checkPanicExists();
 				super.signOut();
 			}
 	}
 
+	//TEST HACKING 2
 	@Test
 	public void test301Hacking() {
-		// HINT: En este test intentamos publicar un curso ya publicado por otro Lecturer distinto.
+		// HINT: En este test intentamos publicar una lecture ya publicado por otro Lecturer distinto.
 
-		Collection<Course> courses;
+		Collection<Lecture> lectures;
 		String params;
 
-		super.signIn("lecturer1", "lecturer1");
-		courses = this.repository.findManyCoursesByLecturerUsername("lecturer1");
-		for (final Course course : courses)
-			if (!course.isDraftMode()) {
-				params = String.format("id=%d", course.getId());
-				super.request("/lecturer/course/publish", params);
+		super.signIn("lecturer2", "lecturer2");
+		lectures = this.repository.findManyLecturesByLecturerUsername("lecturer1");
+		for (final Lecture lecture : lectures)
+			if (!lecture.isDraftMode()) {
+				params = String.format("id=%d", lecture.getId());
+				super.request("/lecturer/lecture/publish", params);
 			}
 		super.signOut();
 	}
 
+	//TEST HACKING 3
 	@Test
 	public void test302Hacking() {
-		// HINT: En este test intentamos publicar un curso que no pertenece al lecturer
+		// HINT: En este test intentamos publicar uns lecture que no pertenece al lecturer
 
-		Collection<Course> courses;
+		Collection<Lecture> lectures;
 		String params;
 
 		super.signIn("lecturer2", "lecturer2");
-		courses = this.repository.findManyCoursesByLecturerUsername("lecturer1");
-		for (final Course course : courses) {
-			params = String.format("id=%d", course.getId());
-			super.request("/lecturer/course/publish", params);
+		lectures = this.repository.findManyLecturesByLecturerUsername("lecturer1");
+		for (final Lecture lecture : lectures) {
+			params = String.format("id=%d", lecture.getId());
+			super.request("/lecturer/lecture/publish", params);
 		}
 		super.signOut();
 	}
