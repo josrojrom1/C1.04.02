9a677ddf742db13da105863038fbe2ff710fa8ea
josej <josej@DESKTOP-2JBAL5I>
2023-05-25 20:45:20 +0200

    Validaciones de Spam sobre Lecture OK!

--- src/main/java/acme/features/lecturer/lecture/LecturerLectureCreateService.java
+++ src/main/java/acme/features/lecturer/lecture/LecturerLectureCreateService.java
@@ -10,12 +10,16 @@ import acme.framework.components.jsp.SelectChoices;
 import acme.framework.components.models.Tuple;
 import acme.framework.services.AbstractService;
 import acme.roles.Lecturer;
+import acme.utility.SpamDetector;
 
 @Service
 public class LecturerLectureCreateService extends AbstractService<Lecturer, Lecture> {
 
 	@Autowired
-	protected LecturerLectureRepository repository;
+	protected LecturerLectureRepository	repository;
+
+	@Autowired
+	protected SpamDetector				textValidator;
 
 
 	@Override
@@ -53,6 +57,29 @@ public class LecturerLectureCreateService extends AbstractService<Lecturer, Lect
 		if (!super.getBuffer().getErrors().hasErrors("learningTime"))
 			// 0.082999 son 5 minutos aproximandamente
 			super.state(object.getLearningTime() >= 1, "learningTime", "lecturer.lecture.form.error.learningTime");
+
+		if (!super.getBuffer().getErrors().hasErrors("title")) {
+			String validar;
+			validar = object.getTitle();
+			super.getBuffer().getErrors().state(super.getRequest(), !this.textValidator.spamChecker(validar), "title", "assistant.tutorial.form.error.spam");
+		}
+		if (!super.getBuffer().getErrors().hasErrors("abst")) {
+			String validar;
+			validar = object.getAbst();
+			super.getBuffer().getErrors().state(super.getRequest(), !this.textValidator.spamChecker(validar), "abst", "assistant.tutorial.form.error.spam");
+		}
+
+		if (!super.getBuffer().getErrors().hasErrors("body")) {
+			String validar;
+			validar = object.getBody();
+			super.getBuffer().getErrors().state(super.getRequest(), !this.textValidator.spamChecker(validar), "body", "assistant.tutorial.form.error.spam");
+		}
+
+		if (!super.getBuffer().getErrors().hasErrors("link")) {
+			String validar;
+			validar = object.getLink();
+			super.getBuffer().getErrors().state(super.getRequest(), !this.textValidator.spamChecker(validar), "link", "assistant.tutorial.form.error.spam");
+		}
 	}
 
 	@Override
