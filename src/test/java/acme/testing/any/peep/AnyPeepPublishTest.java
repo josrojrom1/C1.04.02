6126c71b22f5c9a4447199e05f7aead23e853870
miguelybarra <miguel.ybarra.mym@gmail.com>
2023-05-26 14:47:41 +0200

    añadido spam a peep, y más tests de publishpeep

--- src/test/java/acme/testing/any/peep/AnyPeepPublishTest.java
+++ src/test/java/acme/testing/any/peep/AnyPeepPublishTest.java
@@ -12,28 +12,61 @@ public class AnyPeepPublishTest extends TestHarness {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/peep/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	public void test100Positive(final int recordIndex, final String moment, final String title, final String nick, final String message, final String email, final String link) {
-		// HINT: En este test, listamos las peeps, seleccionamos una de ellas y la publicamos
+		// HINT: En este test, listamos las peeps, creamos una de ellas y la publicamos
 		super.clickOnMenu("Peeps", "List of Peeps");
 		super.checkListingExists();
 		super.clickOnButton("Publish");
 		super.checkFormExists();
-		super.fillInputBoxIn("moment", moment);
 		super.fillInputBoxIn("title", title);
-		//super.fillInputBoxIn("nick", nick);
 		super.fillInputBoxIn("message", message);
 		super.fillInputBoxIn("email", email);
 		super.fillInputBoxIn("link", link);
 		super.clickOnSubmit("Publish");
 		super.checkNotErrorsExist();
+		super.clickOnMenu("Peeps", "List of Peeps");
 		super.checkListingExists();
+		super.sortListing(1, "asc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("message", message);
+		super.checkInputBoxHasValue("email", email);
+		super.checkInputBoxHasValue("link", link);
 
+	}
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/peep/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test101Positive(final int recordIndex, final String moment, final String title, final String nick, final String message, final String email, final String link) {
+		// HINT: En este test, listamos las peeps, creamos una de ellas y la publicamos
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Peeps", "List of Peeps");
+		super.checkListingExists();
+		super.clickOnButton("Publish");
+		super.checkFormExists();
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("nick", nick);
+		super.fillInputBoxIn("message", message);
+		super.fillInputBoxIn("email", email);
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Publish");
+		super.checkNotErrorsExist();
+		super.clickOnMenu("Peeps", "List of Peeps");
+		super.checkListingExists();
+		super.sortListing(1, "asc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("nick", nick);
+		super.checkInputBoxHasValue("message", message);
+		super.checkInputBoxHasValue("email", email);
+		super.checkInputBoxHasValue("link", link);
+
+		super.signOut();
 	}
 
 	//TEST NEGATIVO
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/peep/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	public void test200Negative(final int recordIndex, final String moment, final String title, final String nick, final String message, final String email, final String link) {
-		// HINT: En este test intentamos crear cursos con datos incorrectos.
+		// HINT: En este test intentamos crear peeps con datos incorrectos.
 
 		super.clickOnMenu("Peeps", "List of Peeps");
 		super.checkListingExists();
@@ -41,12 +74,32 @@ public class AnyPeepPublishTest extends TestHarness {
 		super.checkFormExists();
 		super.fillInputBoxIn("moment", moment);
 		super.fillInputBoxIn("title", title);
-		//super.fillInputBoxIn("nick", nick);
 		super.fillInputBoxIn("message", message);
 		super.fillInputBoxIn("email", email);
 		super.fillInputBoxIn("link", link);
 		super.clickOnSubmit("Publish");
 		super.checkErrorsExist();
+	}
+
+	//TEST NEGATIVO
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/peep/publish-logged-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test201Negative(final int recordIndex, final String moment, final String title, final String nick, final String message, final String email, final String link) {
+		// HINT: En este test intentamos crear peeps con datos incorrectos.
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Peeps", "List of Peeps");
+		super.checkListingExists();
+		super.clickOnButton("Publish");
+		super.checkFormExists();
+		super.fillInputBoxIn("moment", moment);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("nick", nick);
+		super.fillInputBoxIn("message", message);
+		super.fillInputBoxIn("email", email);
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Publish");
+		super.checkErrorsExist();
+		super.signOut();
 	}
 
 }
