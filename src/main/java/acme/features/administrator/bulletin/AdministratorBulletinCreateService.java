962ffc7e6776bb2f9fad202c664f24fb06b1be65
josej <josej@DESKTOP-2JBAL5I>
2023-05-26 22:07:01 +0200

    Authorise OK!

--- src/main/java/acme/features/administrator/bulletin/AdministratorBulletinCreateService.java
+++ src/main/java/acme/features/administrator/bulletin/AdministratorBulletinCreateService.java
@@ -32,6 +32,7 @@ public class AdministratorBulletinCreateService extends AbstractService<Administ
 
 	@Override
 	public void authorise() {
+
 		boolean status;
 		status = super.getRequest().getPrincipal().hasRole(Administrator.class);
 		super.getResponse().setAuthorised(status);
