9b6aadc51663708a16c52cf6e2922773649c29c9
pabparmen <pabparmen@alum.us.es>
2023-05-26 22:00:06 +0200

    change authorise administrator

--- src/main/java/acme/features/administrator/banner/AdministratorBannerCreateService.java
+++ src/main/java/acme/features/administrator/banner/AdministratorBannerCreateService.java
@@ -36,7 +36,9 @@ public class AdministratorBannerCreateService extends AbstractService<Administra
 
 	@Override
 	public void authorise() {
-		super.getResponse().setAuthorised(true);
+		boolean status;
+		status = super.getRequest().getPrincipal().hasRole(Administrator.class);
+		super.getResponse().setAuthorised(status);
 	}
 
 	@Override
--- src/main/java/acme/features/administrator/banner/AdministratorBannerShowService.java
+++ src/main/java/acme/features/administrator/banner/AdministratorBannerShowService.java
@@ -27,7 +27,9 @@ public class AdministratorBannerShowService extends AbstractService<Administrato
 
 	@Override
 	public void authorise() {
-		super.getResponse().setAuthorised(true);
+		boolean status;
+		status = super.getRequest().getPrincipal().hasRole(Administrator.class);
+		super.getResponse().setAuthorised(status);
 	}
 
 	@Override
