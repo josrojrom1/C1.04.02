f8e69aa5b953123dd88f8ef8db5eda11bbe103c8
josej <josej@DESKTOP-2JBAL5I>
2023-07-05 18:03:24 +0200

    LecturerCourseCreateTest mejorado OK!

--- src/test/java/acme/testing/lecturer/course/LecturerCourseCreateTest.java
+++ src/test/java/acme/testing/lecturer/course/LecturerCourseCreateTest.java
@@ -1,6 +1,7 @@
 
 package acme.testing.lecturer.course;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -13,7 +14,7 @@ public class LecturerCourseCreateTest extends TestHarness {
 	@CsvFileSource(resources = "/lecturer/course/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	public void test100Positive(final int recordIndex, final String code, final String title, final String abst, final String retailPrice, final String link) {
 		// HINT: Nos autenticamos como Lecturer, mostramos los cursos y hacemos click en crear un curso para ver que todo resulta como esperamos
-		super.signIn("lecturer1", "lecturer1");
+		super.signIn("lecturer2", "lecturer2");
 		super.clickOnMenu("Lecturer", "List my courses");
 		super.checkListingExists();
 		super.sortListing(0, "desc");
@@ -27,8 +28,8 @@ public class LecturerCourseCreateTest extends TestHarness {
 		super.clickOnMenu("Lecturer", "List my courses");
 		super.checkListingExists();
 		super.sortListing(0, "desc");
-		//		super.checkColumnHasValue(recordIndex, 0, code);
-		//		super.checkColumnHasValue(recordIndex, 1, title);
+		super.checkColumnHasValue(recordIndex, 0, code);
+		super.checkColumnHasValue(recordIndex, 1, title);
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
 		super.checkInputBoxHasValue("code", code);
@@ -40,43 +41,43 @@ public class LecturerCourseCreateTest extends TestHarness {
 	}
 
 	//TEST NEGATIVO
-	//	@ParameterizedTest
-	//	@CsvFileSource(resources = "/lecturer/course/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	//	public void test200Negative(final int recordIndex, final String code, final String title, final String abst, final String retailPrice, final String link) {
-	//		// HINT: En este test intentamos crear cursos con datos incorrectos.
-	//
-	//		super.signIn("lecturer1", "lecturer1");
-	//		super.clickOnMenu("Lecturer", "List my courses");
-	//		super.clickOnButton("Create new course");
-	//		super.checkFormExists();
-	//		super.fillInputBoxIn("code", code);
-	//		super.fillInputBoxIn("title", title);
-	//		super.fillInputBoxIn("abst", abst);
-	//		super.fillInputBoxIn("retailPrice", retailPrice);
-	//		super.fillInputBoxIn("link", link);
-	//		super.clickOnSubmit("Create");
-	//		super.checkErrorsExist();
-	//		super.signOut();
-	//	}
-	//
-	//	//TEST HACKING
-	//	@Test
-	//	public void test300Hacking() {
-	//		// HINT: Este test intenta crear un curso usando principals con roles inapropiados
-	//
-	//		super.checkLinkExists("Sign in");
-	//		super.request("/lecturer/course/create");
-	//		super.checkPanicExists();
-	//
-	//		super.signIn("administrator", "administrator");
-	//		super.request("/lecturer/course/create");
-	//		super.checkPanicExists();
-	//		super.signOut();
-	//
-	//		super.signIn("assistant1", "assistant1");
-	//		super.request("/lecturer/course/create");
-	//		super.checkPanicExists();
-	//		super.signOut();
-	//	}
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/course/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int recordIndex, final String code, final String title, final String abst, final String retailPrice, final String link) {
+		// HINT: En este test intentamos crear cursos con datos incorrectos.
+
+		super.signIn("lecturer1", "lecturer1");
+		super.clickOnMenu("Lecturer", "List my courses");
+		super.clickOnButton("Create new course");
+		super.checkFormExists();
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("abst", abst);
+		super.fillInputBoxIn("retailPrice", retailPrice);
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Create");
+		super.checkErrorsExist();
+		super.signOut();
+	}
+
+	//TEST HACKING
+	@Test
+	public void test300Hacking() {
+		// HINT: Este test intenta crear un curso usando principals con roles inapropiados
+
+		super.checkLinkExists("Sign in");
+		super.request("/lecturer/course/create");
+		super.checkPanicExists();
+
+		super.signIn("administrator", "administrator");
+		super.request("/lecturer/course/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("assistant1", "assistant1");
+		super.request("/lecturer/course/create");
+		super.checkPanicExists();
+		super.signOut();
+	}
 
 }
