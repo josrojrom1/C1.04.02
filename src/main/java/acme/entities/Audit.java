9f0d9ce7968a08678a3b6828797989b01fb32332
Josreimun <josreimun@alum.us.es>
2023-07-06 13:30:56 +0200

    Corregidos los errores de validacion en las operaciones sobre audits

--- src/main/java/acme/entities/Audit.java
+++ src/main/java/acme/entities/Audit.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
@@ -56,6 +57,7 @@ public class Audit extends AbstractEntity {
 
 	@ManyToOne()
 	@Valid
+	@NotNull
 	protected Course			course;
 
 	protected boolean			draftMode;
